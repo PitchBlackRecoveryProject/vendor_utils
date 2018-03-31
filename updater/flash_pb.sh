0deb8da7d25dee978866dfc6321c8440e62334b0
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-03-31 12:40:08 +0530

    flash_pb.sh: Added Scirpt to Find Recovery
    * Recovery Partition of Any Chipset

--- updater/flash_pb.sh
+++ updater/flash_pb.sh
@@ -2,15 +2,42 @@
 # set up extracted files and directories
 PB=/tmp/pb/PBTWRP
 IMG=/tmp/pb/TWRP/recovery.img
-RECOVERY=/dev/block/bootdevice/by-name/recovery
+RECOVERY=recovery
 ETC=/system/etc/install-recovery.sh
 PB1_PATH=/sdcard/Android/PBTWRP
 PB2_PATH=/sdcard/TWRP/PBTWRP
 UI=/sdcard/TWRP/theme/ui.zip
 RES=/sdcard/TWRP/.twrps
+red='\033[0;31m'
+
+recovery_partition() {
+if [ -f /etc/recovery.fstab ]; then
+		# recovery fstab v1
+		RECOVERY=$(awk '$1 == "/recovery" {print $3}' /etc/recovery.fstab)
+		if [ -z "$RECOVERY" ]; then
+				# recovery fstab v2
+		RECOVERY=$(awk '$2 == "/recovery" {print $1}' /etc/recovery.fstab)
+		fi
+	fi
+	for fstab in /fstab.*; do
+		[ -f "$fstab" ] || continue
+		# device fstab v2
+		RECOVERY=$(awk '$2 == "/recovery" {print $1}' "$fstab")
+		if [ -n "$RECOVERY" ]; then
+			break
+		else
+		# device fstab v1
+		RECOVERY=$(awk '$1 == "/recovery" {print $3}' "$fstab")
+		fi
+		
+	done
+	if [ -z "`echo $RECOVERY | grep "recovery"`" ]; then
+	echo "$redFailed to Fined RECOVERY Partition";
+	exit 1;
+	fi
+}
 
 #Deletion
-
 if [ -f $ETC ]; then
 	rm /system/etc/install-recovery.sh
 fi
@@ -28,6 +55,7 @@ if [ -f $RES ]; then
 fi
 
 #Flashing
+recovery_partition;
 flash_image $RECOVERY $IMG
 
 #Copy Specific Files
