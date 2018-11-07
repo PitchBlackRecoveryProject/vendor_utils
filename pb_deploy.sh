385014562e6980034167518278f32fd3ead143ea
Manjot Sidhu <manjot.techie@gmail.com>
2018-11-07 10:16:24 +0530

    pb_deploy: remove that crap shit
    Install sshpass yourself, I simply don't care ...

--- pb_deploy.sh
+++ pb_deploy.sh
@@ -24,9 +24,6 @@ nocol='\033[0m'
 purple='\e[0;35m'
 white='\e[0;37m'
 
-# Install sshpass if not installed
-sudo apt-get install sshpass -y >nul 2>nul
-
 read -p "Enter build device codename:" codename
 read -p "Enter build date(YYYYMMDD):" build
 read -p "Enter location of the build to upload:" sf_file
