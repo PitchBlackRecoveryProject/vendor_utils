418d05a084757e225a3bc9795bbcdf8913b20630
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2019-11-05 20:09:38 +0000

    Update build.sh
    ★ Use only "fr3akyphantom/pitchblack-builder:latest" Docker Container, built by me. It is based on Bionic and contains openjdk8, repo, ghr & other dependencies.
    ★ In newer builds, use the "jobs.build.environment" key to setup device-related variables in _.circleci/config.yml_
    ★ In later builds, in the _config.yml_, summon this _build.sh_ right after github setup. Don't add any unnecessary _apt-get update_ or _apt-get install_

--- build.sh
+++ build.sh
@@ -1,29 +1,21 @@
+#!/usr/bin/env bash
+
 SFUserName=$1
 SFPassword=$2
 GITHUB_TOKEN=$3
 
-PATH=~/bin:$PATH
-curl --create-dirs -L -o ~/bin/repo -O -L https://github.com/akhilnarang/repo/raw/master/repo
-chmod a+x ~/bin/repo
-
-echo "Install ghr binary"
-wget -q 'https://github.com/tcnksm/ghr/releases/download/v0.13.0/ghr_v0.13.0_linux_amd64.tar.gz'
-tar -xzf ghr_v0.13.0_linux_amd64.tar.gz && rm ghr_v0.13.0_linux_amd64.tar.gz
-cp ghr_v0.13.0_linux_amd64/ghr ~/bin && PATH=~/bin:$PATH
-rm -rf ghr_v0.13.0_linux_amd64/
-which ghr
-which repo
+echo -en "The Whole PATH ENV is - " && echo $PATH
+which ghr && which repo
 
 echo "Initialize & Sync PBRP repo"
 echo $(pwd)
-mkdir $(pwd)/work && cd work
-repo init --depth=1 -q -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b ${MANIFEST_BRANCH}
+repo init -q -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b ${MANIFEST_BRANCH} --depth 1
 time repo sync -c -f -q --force-sync --no-clone-bundle --no-tags -j32
 
 echo "Get the Device Tree on place"
 git clone https://$GITHUB_TOKEN@github.com/PitchBlackRecoveryProject/${CIRCLE_PROJECT_REPONAME} device/${VENDOR}/${CODENAME}
 
-# Sync again, as Device has Dependencies
+# Sync again, as Device may have Dependencies
 time repo sync -c -f -q --force-sync --no-clone-bundle --no-tags -j32
 
 # Keep the whole .repo/manifests folder
@@ -32,7 +24,7 @@ echo "Clean up the .repo, no use of it now"
 rm -rf .repo
 mkdir -p .repo && mv manifests .repo/ && ls -la .repo/*
 
-rm -rf bootable/recovery && git clone https://github.com/PitchBlackRecoveryProject/android_bootable_recovery -b ${PBRP_BRANCH} bootable/recovery
+rm -rf bootable/recovery && git clone https://github.com/PitchBlackRecoveryProject/android_bootable_recovery -b ${PBRP_BRANCH} --single-branch bootable/recovery
 rm -rf vendor/pb && git clone https://github.com/PitchBlackRecoveryProject/vendor_pb -b pb vendor/pb
 
 echo "Start the Build Process"
