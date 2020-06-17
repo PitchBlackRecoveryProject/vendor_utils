dd1911b623d6ec1964a48ee14ae9e6e9cc0746db
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-06-17 19:22:22 +0600

    vendor_pb: (1/2) Reconfigure build system
    - Make the build script NOOB-friendly as-well-as Dev-specific
    - Fix the TG Notification for Maintainers only
    
    Co-authored-by: MD Raza <raza231198@gmail.com>
    Signed-off-by: rokibhasansagar <rokibhasansagar2014@outlook.com>
    Change-Id: I48aab3cb7353f97e786ee08a21122eafc7c97686
    Signed-off-by: rokibhasansagar <rokibhasansagar2014@outlook.com>

--- build.sh
+++ build.sh
@@ -22,22 +22,45 @@
 #
 ###
 
+###
+# USE 4-CHARACTER-SPACE IN PLACE OF TAB CHARACTER FOR EDITING
+###
+
+# SANITY CHECKS
+if [[ -z $GitHubMail ]]; then echo -e "You haven't configured GitHub E-Mail Address." && exit 1; fi
+if [[ -z $GitHubName ]]; then echo -e "You haven't configured GitHub Username." && exit 1; fi
+if [[ -z $GITHUB_TOKEN ]]; then echo -e "You haven't configured GitHub Token.\nWithout it, recovery can't be published." && exit 1; fi
+if [[ -z $MANIFEST_BRANCH ]]; then echo -e "You haven't configured PitchBlack Recovery Project Manifest Branch." && exit 1; fi
+if [[ -z $VENDOR ]]; then echo -e "You haven't configured Vendor name." && exit 1; fi
+if [[ -z $CODENAME ]]; then echo -e "You haven't configured Device Codename." && exit 1; fi
+if [[ -z $BUILD_LUNCH ]] && [[ -z $FLAVOR ]]; then echo -e "Set at least one variable. BUILD_LUNCH or FLAVOR." && exit 1; fi
+
+# Set GitAuth Infos"
+git config --global user.email $GitHubMail
+git config --global user.name $GitHubName
+git config --global credential.helper store
+git config --global color.ui true
+
 echo -e "Starting the CI Build Process...\n"
 
 DIR=$(pwd)
 mkdir $(pwd)/work && cd work
 
-echo -e "\nInitializing PBRP repo sync..."
-echo $(pwd)
-repo init -q -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b ${MANIFEST_BRANCH} --depth 1
+echo -e "Initializing PBRP repo sync..."
+repo init -q -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b ${MANIFEST_BRANCH} --depth=1
 time repo sync -c -q --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
-rm -rf vendor/pb
-git clone https://github.com/PitchBlackRecoveryProject/vendor_pb -b pb vendor/pb --depth=1
+# CLONE VENDOR REPO AGAIN FOR SAFEKEEPING
+rm -rf vendor/pb && git clone https://github.com/PitchBlackRecoveryProject/vendor_pb -b pb vendor/pb --depth=1
 
 echo -e "\nGetting the Device Tree on place"
-git clone --quiet --progress https://$GitHubName:$GITHUB_TOKEN@github.com/PitchBlackRecoveryProject/${CIRCLE_PROJECT_REPONAME} -b ${CIRCLE_BRANCH} device/${VENDOR}/${CODENAME}
+if [[ "${CIRCLE_PROJECT_USERNAME}" == "PitchBlackRecoveryProject" ]]; then
+    git clone --quiet --progress https://$GitHubName:$GITHUB_TOKEN@github.com/PitchBlackRecoveryProject/${CIRCLE_PROJECT_REPONAME} -b ${CIRCLE_BRANCH} device/${VENDOR}/${CODENAME}
+else
+    git clone https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} -b ${CIRCLE_BRANCH} device/${VENDOR}/${CODENAME}
+fi
 
 if [[ -n ${USE_SECRET_BOOTABLE} ]]; then
+    # ONLY FOR CORE DEVS
     if [[ -n ${PBRP_BRANCH} ]]; then
         unset PBRP_BRANCH
     fi
@@ -47,19 +70,24 @@ if [[ -n ${USE_SECRET_BOOTABLE} ]]; then
     rm -rf bootable/recovery
     git clone --quiet --progress https://$GitHubName:$GITHUB_TOKEN@github.com/PitchBlackRecoveryProject/pbrp_recovery_secrets -b ${SECRET_BR} --single-branch bootable/recovery
 elif [[ -n ${PBRP_BRANCH} ]]; then
+    # FOR EVERYBODY
     rm -rf bootable/recovery
     git clone --quiet --progress https://github.com/PitchBlackRecoveryProject/android_bootable_recovery -b ${PBRP_BRANCH} --single-branch bootable/recovery
 fi
 
 if [[ -n $EXTRA_CMD ]]; then
-    eval $EXTRA_CMD
+    eval "$EXTRA_CMD"
     cd $DIR/work
 fi
 
 echo -e "\nPreparing Delicious Lunch..."
 export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh
-lunch ${BUILD_LUNCH}
+if [[ -n $BUILD_LUNCH ]]; then
+    lunch ${BUILD_LUNCH}
+elif [[ -n $FLAVOR ]]; then
+    lunch omni_${CODENAME}-${FLAVOR}
+fi
 
 # Keep the whole .repo/manifests folder
 cp -a .repo/manifests $(pwd)/
@@ -83,9 +111,10 @@ if [[ -n ${BUILD_FILE_TAR} ]]; then
     cp ${BUILD_FILE_TAR} ${UPLOAD_PATH}
 fi
 
-if [[ -n $BUILDFILE ]]; then
+if [[ "${CIRCLE_PROJECT_USERNAME}" == "PitchBlackRecoveryProject" ]] && [[ -n $BUILDFILE ]]; then
     echo "Got the Official Build: $BUILDFILE"
-    sudo chmod a+x vendor/pb/pb_deploy.sh && ./vendor/pb/pb_deploy.sh ${CODENAME} ${SFUserName} ${SFPassword} ${GITHUB_TOKEN} ${VERSION} ${MAINTAINER}
+    sudo chmod a+x vendor/pb/pb_deploy.sh
+    ./vendor/pb/pb_deploy.sh ${CODENAME} ${SFUserName} ${SFPassword} ${GITHUB_TOKEN} ${VERSION} ${MAINTAINER}
     cp $BUILDFILE $UPLOAD_PATH
     export BUILDFILE=$(find $(pwd)/out/target/product/${CODENAME}/recovery.img 2>/dev/null)
     cp $BUILDFILE $UPLOAD_PATH
@@ -96,11 +125,20 @@ elif [[ $TEST_BUILD == 'true' ]] && [[ -n $TEST_BUILDFILE ]]; then
     export TEST_BUILDIMG=$(find $(pwd)/out/target/product/${CODENAME}/recovery.img 2>/dev/null)
     cp $TEST_BUILDIMG $UPLOAD_PATH
     ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -n "Test Release for $(echo $CODENAME)" -b "PBRP $(echo $VERSION)" -c ${CIRCLE_SHA1} -delete ${VERSION}-test ${UPLOAD_PATH}
+else
+    echo -e "Something Wrong with your build system.\nPlease fix it." && exit 1
+fi
+
+# SEND NOTIFICATION TO MAINTAINERS, AVAILABLE FOR TEAM DEVS ONLY
+if [[ "${CIRCLE_PROJECT_USERNAME}" == "PitchBlackRecoveryProject" ]]; then
     echo -e "\nSending the Test build info in Maintainer Group\n"
-    TEST_LINK="https://github.com/PitchBlackRecoveryProject/${CIRCLE_PROJECT_REPONAME}/releases/download/${VERSION}/$(echo $TEST_BUILDFILE | awk -F'[/]' '{print $NF}')"
+    TEST_LINK="https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/releases/download/${VERSION}-test/$(echo $TEST_BUILDFILE | awk -F'[/]' '{print $NF}')"
     MAINTAINER_MSG="PitchBlack Recovery for ${VENDOR} ${CODENAME} is available Only For Testing Purpose\n\n"
+    if [[ ! -z $MAINTAINER ]]; then MAINTAINER_MSG=${MAINTAINER_MSG}"Maintainer: ${MAINTAINER}\n\n"; fi
+    if [[ ! -z $CHANGELOG ]]; then MAINTAINER_MSG=${MAINTAINER_MSG}"Changelog:\n"${CHANGELOG}"\n"; fi
     MAINTAINER_MSG=${MAINTAINER_MSG}"Go to ${TEST_LINK} to download it."
-    cd vendor/pb; python3 telegram.py -c "-1001228903553" -M "$MAINTAINER_MSG"; cd -
+    cd vendor/pb; python3 telegram.py -c "-1001228903553" -m "HTML" -M "$MAINTAINER_MSG"; cd $DIR/work
 fi
 
 echo -e "\n\nAll Done Gracefully\n\n"
+
