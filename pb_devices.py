6552e1c521d18c743cfbb3c602863371e90cd620
Manjot Sidhu <manjot.techie@gmail.com>
2020-07-19 15:37:59 +0530

    PBRP: pb_devices script: dump releasing on json [1/2].
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>

--- pb_devices.py
+++ pb_devices.py
@@ -9,15 +9,10 @@
 import sys, json, urllib.request, shutil, os
 
 url = 'https://raw.githubusercontent.com/PitchBlackRecoveryProject/vendor_utils/pb/pb_devices.json'
-local_file = 'pb_devices.json'
-backup_file = 'pb_devices.json.bk'
 arguments = sys.argv
 
 def invalid_arguments():
-	raise Exception("PB_DEVICES.PY: Use proper arguments.\n\n Available Commands:\n > pb_devices.py verify <vendor> <codename>.\n > pb_devices.py release <vendor> <codename> <build-date-time>")
-
-def release_error(error_message):
-	raise Exception("PB_DEVICES.PY: ", error_message)
+	raise Exception("PB_DEVICES.PY: Use proper arguments.\n\n Available Commands:\n > pb_devices.py verify <vendor> <codename>.")
 
 
 def verify_device(vendor, codename, maintainer = 0):
@@ -68,58 +63,18 @@ def verify_device(vendor, codename, maintainer = 0):
 
 	return found
 
-def release(vendor, codename, build_date_time):
-	shutil.copyfile(local_file, backup_file)
-
-	with open(backup_file, 'r') as f:
-	  data = json.load(f)
-
-	ven = data.keys()
-	for i in ven:
-		if i.casefold() == vendor.casefold():
-			ven = i
-			break
-	if not ven:
-		return 1
-
-	cod = data[ven]
-
-	for i in cod:
-		if i.casefold() == codename.casefold():
-			cod = i
-			break
-	if not cod or isinstance(cod, str) is False:
-		return 1
-
-	if codename.casefold() == cod.casefold():
-		data[ven][cod]['latest_release'] = build_date_time
-	else:
-  		release_error('Device not Official')
-
-	with open(local_file, 'w') as f:
-	  json.dump(data, f, indent=2)
-
-	os.remove(backup_file)
-	print("PB_DEVICES.PY: Release Success! ", codename, " ", build_date_time)
 
 if len(arguments) < 4:
 	invalid_arguments()
 
 cmd = arguments[1]
+
 if len(arguments) != 5 or cmd != 'verify':
 	print("PB_DEVICES: Detected Codename: ", arguments[3])
 	print("PB_DEVICES: Detected Vendor: ", arguments[2])
-
 if cmd == 'verify' and len(arguments) == 4:
 	exit(verify_device(arguments[2], arguments[3]))
-
 elif cmd == 'verify' and len(arguments) == 5:
 	exit(verify_device(arguments[2], arguments[3], arguments[4]))
-
-elif cmd == 'release':
-	if len(arguments) < 5:
-		invalid_arguments()
-
-	release(arguments[2], arguments[3], arguments[4])
 else:
 	invalid_arguments()
