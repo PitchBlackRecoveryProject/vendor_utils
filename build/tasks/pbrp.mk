b171e9fc6cc182c048f7fc7f3158443d44c27ad5
Mohd Faraz <androiabledroid@gmail.com>
2022-12-31 14:55:24 +0100

    [WIP]pb: update installer
     * inline with osmOsis installer
    
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- build/tasks/pbrp.mk
+++ build/tasks/pbrp.mk
@@ -1,4 +1,5 @@
 WORK_PATH := $(OUT_DIR)/target/product/$(TARGET_DEVICE)/zip
+TARGET_DIR := $(WORK_PATH)/../
 BUILD_TOP := $(OUT_DIR)/../
 VERSION := $(shell cat $(BUILD_TOP)/bootable/recovery/variables.h | egrep "define\s+PB_MAIN_VERSION" | tr -d '"' | tr -s ' ' | awk '{ print $$3 }')
 PB_VENDOR := vendor/utils
@@ -17,36 +18,35 @@ else
 endif
 
 ZIP_NAME := PBRP-$(TARGET_DEVICE)-$(VERSION)-$(shell date +%Y%m%d-%H%M)-$(PB_BUILD_TYPE).zip
-RECOVERYPATH := $(OUT_DIR)/target/product/$(TARGET_DEVICE)/recovery.img
-KEYCHECK := $(OUT_DIR)/recovery/root/sbin/keycheck
+KEYCHECK := $(TARGET_DIR)/recovery/root/sbin/keycheck
 AB := false
-ifeq ($(AB_OTA_UPDATER), true)
-    AB := true
-endif
 ifeq ($(BOARD_USES_RECOVERY_AS_BOOT), true)
     RECOVERY_AS_BOOT := true
+    RECOVERYPATH := $(OUT_DIR)/target/product/$(TARGET_DEVICE)/ramdisk-recovery.cpio
     pbrpimage=$(INSTALLED_BOOTIMAGE_TARGET) $(RECOVERY_RESOURCE_ZIP)
 else
+    RECOVERYPATH := $(OUT_DIR)/target/product/$(TARGET_DEVICE)/recovery.img
+    RECOVERY_AS_BOOT := false
     pbrpimage=$(INSTALLED_RECOVERYIMAGE_TARGET) $(RECOVERY_RESOURCE_ZIP)
 endif
 
 .PHONY: pbrp
 pbrp: $(pbrpimage)
-	$(hide) rm -f $(WORK_PATH)/../*.zip
-	$(hide) if [ -d $(WORK_PATH) ]; then rm -rf $(WORK_PATH); fi
-	$(hide) mkdir $(WORK_PATH)
-	$(hide) rsync -avp $(PB_VENDOR)/PBRP $(WORK_PATH)/
+	$(hide) rm -f $(TARGET_DIR)/*.zip
+	$(hide) rm -rf $(WORK_PATH) && mkdir $(WORK_PATH);
+	$(hide) rsync -avp $(PB_VENDOR)/PBRP $(WORK_PATH)/;
 	$(hide) mkdir -p $(WORK_PATH)/META-INF/com/google/android
 	$(hide) rsync -avp $(PB_VENDOR)/updater/update-* $(WORK_PATH)/META-INF/com/google/android/
+	$(hide) sed -i "s:{version}:v$(VERSION):g" $(WORK_PATH)/META-INF/com/google/android/update-binary
+	echo "dsfsdfsdf"
+	$(hide) if [ "$(RECOVERY_AS_BOOT)" == "true" ]; then sed -i "s:IS_AB=false:IS_AB=true:" $(WORK_PATH)/META-INF/com/google/android/update-binary; fi
+	echo "dsfsdfsdf2255"
 	$(hide) rsync -avp $(PB_VENDOR)/updater/awk $(WORK_PATH)/META-INF/
-	$(hide) rsync -avp $(PB_VENDOR)/updater/magiskboot $(WORK_PATH)/
+	$(hide) rsync -avp $(BUILD_TOP)/external/magisk-prebuilt/prebuilt/magiskboot_arm $(WORK_PATH)/magiskboot;
+	$(hide) chmod +x $(WORK_PATH)/magiskboot;
 	$(hide) if [ -f $(KEYCHECK) ]; then cp $(KEYCHECK) $(WORK_PATH)/META-INF/; fi
-	$(hide) if [ "$(AB)" == "true" ]; then sed -i "s|AB_DEVICE=false|AB_DEVICE=true|g" $(WORK_PATH)/META-INF/com/google/android/update-binary; fi
-	$(hide) if [ "$(RECOVERY_AS_BOOT)" == "true" ]; then sed -i "s|USES_RECOVERY_AS_BOOT=false|USES_RECOVERY_AS_BOOT=true|g" $(WORK_PATH)/META-INF/com/google/android/update-binary; fi
-	$(hide) mkdir $(WORK_PATH)/TWRP
-	$(hide) if [ -e $(WORK_PATH)/../boot.img ]; then cp $(WORK_PATH)/../boot.img $(WORK_PATH)/../recovery.img; fi
-	$(hide) cp $(WORK_PATH)/../recovery.img $(WORK_PATH)/TWRP/
-	$(hide) cd $(WORK_PATH) && zip -r $(ZIP_NAME) *
+	$(hide) cp $(RECOVERYPATH) $(WORK_PATH)/;
+	$(hide) cd $(WORK_PATH) && zip -r $(ZIP_NAME) *;
 	$(hide) cd $(BUILD_TOP) && mv $(WORK_PATH)/$(ZIP_NAME) $(WORK_PATH)/../
 	$(hide) cat $(BUILD_TOP)/vendor/utils/.pb.1
 	printf "Recovery Image: %s\n" "$(RECOVERYPATH)"
